e59610e71d9c1422253d364a83b0fe1e6ce9eff4
myothandaraung <myothandaraung713@gmail.com>
2025-07-25 16:39:18 +0630

    change bmi calculator

--- app/pages/bmi_calculator.vue
+++ app/pages/bmi_calculator.vue
@@ -2,21 +2,41 @@
 import { ref } from 'vue'
 import { mealPlans } from '~/data/mealPlans'
 
-const sex = ref('female')
-const age = ref(21)
-const height = ref(170)
+const sex = ref('')
+const age = ref(0)
+const height = ref(0)
 const heightUnit = ref('cm')
-const weight = ref()
+const weight = ref(0)
 const bmi = ref(null)
 const feet = ref(5)
 const inches = ref(10)
 const bmiCategory = ref('');
 const weightUnit = ref('kg');
-const selectedGoal = ref('');
+//const selectedGoal = ref('');
 const tdee = ref(null);
+const displayMealPlan = ref(false)
 
 
+const errorFields = ref({
+  sex: false,
+  age: false,
+  height: false,
+  weight: false
+})
 const calculateBMI = () => {
+    displayMealPlan.value = false;
+    errorFields.value = {
+        sex: sex.value === '',
+        age: age.value <= 0,
+        height:
+            heightUnit.value === 'cm'
+                ? !height.value || height.value <= 0
+                : (feet.value <= 0 && inches.value <= 0),
+        weight: !weight.value || weight.value <= 0
+    }
+
+  // Prevent calculation if any error exists
+  if (Object.values(errorFields.value).includes(true)) return;
     console.log(weightUnit.value)
     let weight_data = weight.value;
     let heightInMeters = heightUnit.value === 'cm' ? height.value / 100 : (feet.value * 0.3048) + (inches.value * 0.0254)
@@ -27,18 +47,6 @@ const calculateBMI = () => {
         }
     }
     bmi.value = weight_data / (heightInMeters * heightInMeters)
-    // if (heightUnit.value === 'cm') {
-    //     if (height.value > 0) {
-    //         const heightInMeters = height.value / 100
-    //         bmi.value = weight_data / (heightInMeters * heightInMeters)
-    //     }
-    // } else {
-    //     if (feet.value > 0 && inches.value > 0) {
-    //         const heightInMeters = (feet.value * 0.3048) + (inches.value * 0.0254)
-    //         bmi.value = weight_data / (heightInMeters * heightInMeters)
-    //     }
-    // }
-
     if (bmi.value) {
         if (bmi.value < 18.5) {
             bmiCategory.value = 'Underweight'
@@ -62,8 +70,6 @@ const calculateTDEE = (weightKg, heightCm, age, sex) => {
       ? 3.4 * weightKg + 15.3 * heightCm - 6.8 * age - 961
       : 2.4 * weightKg + 9.3 * heightCm - 4.7 * age - 65
     }
-    // console.log(bmr)
-    // const activityMultiplier = 1.4 // Light activity
     tdee.value = Math.round(tdee.value)
 }
 const goals = [
@@ -79,6 +85,7 @@ const goal = computed(() => {
   if (bmiCategory.value === 'Normal weight') return 'maintain'
   if (bmiCategory.value === 'Obesity') return 'lose'
 })
+const selectedGoal = useState('goal', () => goal.value)
 const dailyCalories = computed(() => {
     console.log(tdee.value)
   if (goal.value === 'gain') return tdee.value + 500
@@ -100,7 +107,7 @@ const inactiveClass =
   
         <div class="grid grid-cols-1 md:grid-cols-2 gap-6 mb-6">
             <!-- 1. Sex -->
-            <div class="bg-gray-50 p-4 rounded-lg shadow-sm mb-6">
+            <div class="bg-gray-50 p-4 rounded-lg shadow-sm mb-6 border" :class="errorFields.sex ? 'border-red-500' : 'border-gray-300'">
                 <div class="mb-2 font-semibold">1. What is your sex?</div>
                 <div class="flex space-x-4">
                     <button
@@ -119,7 +126,7 @@ const inactiveClass =
             </div>
   
             <!-- 2. Age -->
-            <div class="bg-gray-50 p-4 rounded-lg shadow-sm mb-6">
+            <div class="bg-gray-50 p-4 rounded-lg shadow-sm mb-6 border" :class="errorFields.age ? 'border-red-500' : 'border-gray-300'">
                 <div class="mb-2 font-semibold">2. How old are you?</div>
                 <input
                     v-model.number="age"
@@ -131,7 +138,7 @@ const inactiveClass =
             </div>
   
             <!-- 3. Height -->
-            <div class="bg-gray-50 p-4 rounded-lg shadow-sm">
+            <div class="bg-gray-50 p-4 rounded-lg shadow-sm border" :class="errorFields.height ? 'border-red-500' : 'border-gray-300'">
                 <div class="mb-2 font-semibold">3. How tall are you?</div>
                 <input
                     v-if="heightUnit === 'cm'"
@@ -165,7 +172,7 @@ const inactiveClass =
             </div>
   
             <!-- 4. Weight -->
-            <div class="bg-gray-50 p-4 rounded-lg shadow-sm">
+            <div class="bg-gray-50 p-4 rounded-lg shadow-sm border" :class="errorFields.weight ? 'border-red-500' : 'border-gray-300'">
                 <div class="mb-2 font-semibold">4. How much do you weigh?</div>
                 <input
                     v-model.number="weight"
@@ -189,57 +196,6 @@ const inactiveClass =
             >
                 Calculate your BMI
             </button>
-  
-            <!-- <div v-if="bmi" class="mt-4 text-xl font-semibold text-blue-600">
-                Your BMI is: {{ bmi.toFixed(1) }}
-            </div>
-            <div v-if="bmiCategory" class="mt-4 text-xl font-semibold text-blue-600">
-                Your BMI Category is: {{ bmiCategory }}
-            </div> -->
-            <!-- <div v-if="bmi && bmiCategory" class="max-w-2xl mx-auto space-y-6 mt-10">
-
-                <div class="bg-green-100 rounded-xl p-6 shadow">
-                    <div class="flex justify-between items-center mb-2">
-                        <h2 class="text-xl font-semibold text-gray-700">Your BMI:</h2>
-                        <span class="text-2xl font-bold text-green-700">{{ bmi.toFixed(1) }}</span>
-                    </div>
-                    <p class="text-gray-800 font-medium">
-                        According to your BMI, you might be <span class="font-semibold" :class="bmiCategory === 'Normal weight' ? 'text-green-600' : 'text-red-600'">{{bmiCategory}}</span>.
-                    </p>
-                    <hr class="my-3" />
-                    <p class="text-sm text-gray-600">
-                        <strong class="text-gray-800">Our recommendation:</strong><br />
-                        Try to gain some weight in a healthy manner until you weigh between 
-                        <span class="font-semibold">125.4 lb</span> and <span class="font-semibold">160.3 lb</span>.
-                        <br />And we can help you do just that!
-                    </p>
-                </div>
-
-                <div class="flex items-center space-x-4">
-                    <img src="https://img.icons8.com/emoji/48/apple-emoji.png" alt="mascot" class="w-10 h-10" />
-                    <div class="bg-white shadow px-4 py-2 rounded-full text-gray-800 font-semibold">
-                        What’s your main goal?
-                    </div>
-                </div>
-
-                <div class="bg-teal-50 rounded-xl p-4">
-                    <p class="text-center text-teal-700 font-medium mb-4">Recommended for you</p>
-
-                    <div class="space-y-3">
-                        <div
-                            v-for="(goal, i) in goals"
-                            :key="i"
-                            class="flex items-center justify-between p-4 bg-white rounded-lg shadow-sm cursor-pointer hover:bg-gray-50"
-                            :class="{ 'ring-2 ring-teal-400': selectedGoal === goal.label }"
-                            @click="selectedGoal = goal.label"
-                        >
-                            <span class="text-lg">
-                                <span class="mr-2">{{ goal.icon }}</span>{{ goal.label }}
-                            </span>
-                        </div>
-                    </div>
-                </div>
-            </div> -->
             <div class="max-w-3xl mx-auto py-6 space-y-6" v-if="bmiCategory">
                 <div class="text-center">
                     <h2 class="text-3xl font-bold">Your BMI is {{ bmi.toFixed(1) }}</h2>
@@ -250,8 +206,12 @@ const inactiveClass =
                         Recommended Daily Calorie Intake: <strong>{{ dailyCalories }} kcal</strong>
                     </p>
                 </div>
+                <NuxtImg densities="x1" src="/images/bmi-group.jpg" format="webp" alt="" />
+                <h4>You want to get our weekly meal plan?</h4>
+                <p>Click the button below to get your weekly meal plan</p>
+                <button @click="displayMealPlan = true" class="bg-gradient-to-r from-pink-500 to-orange-400 text-white font-bold py-3 px-6 rounded-full shadow-lg hover:opacity-90 transition">Get Weekly Meal Plan</button>
 
-                <div class="bg-white shadow rounded p-6">
+                <div class="bg-white shadow rounded p-6" v-if="displayMealPlan">
                     <h3 class="text-2xl font-semibold mb-4">Weekly Meal Plan ({{ goal }})</h3>
                     <div class="space-y-3">
                         <div v-for="(dayMeals, day) in meals" :key="day" class="border-b pb-2">
